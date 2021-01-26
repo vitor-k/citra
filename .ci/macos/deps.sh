0a12103897c8619bcd702e57482d12ad1c8dbf65
Vitor K <vitor-kiguchi@hotmail.com>
2021-01-26 19:16:01 -0300

    Use || true to ignore brew installation errors

--- .ci/macos/deps.sh
+++ .ci/macos/deps.sh
@@ -3,6 +3,5 @@
 brew update
 brew unlink python@2 || true
 rm '/usr/local/bin/2to3' || true
-brew install p7zip llvm || true
-brew install qt5 sdl2 ccache ffmpeg ninja
+brew install qt5 sdl2 p7zip ccache ffmpeg llvm ninja || true
 pip3 install macpack
